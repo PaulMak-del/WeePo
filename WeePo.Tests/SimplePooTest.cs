0db34cdc2eba8d5729a9774c2420b7967a260b4e
Павел Хоменко <pawelxom@mail.ru>
2026-03-29 00:11:03 +0300

    ci(build): add test coverage to ci

---
+++ WeePo.Tests/SimplePooTest.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using Xunit;
+
+namespace WeePo.Tests;
+
+[TestSubject(typeof(SimplePoo))]
+public class SimplePooTest
+{
+
+    [Fact]
+    public void SimpleTest()
+    {
+        SimplePoo instance = new();
+        int result = instance.SumOfTwo(5, 4);
+        
+        Assert.Equal(9, result);
+    }
+}
---
+++ WeePo/SimplePoo.cs
@@ -0,0 +1,14 @@
+﻿namespace WeePo;
+
+public class SimplePoo
+{
+    public int SumOfTwo(int a, int b)
+    {
+        return a + b; 
+    }
+
+    public int SumOfThree(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
